67113cb5fdb1ce1b138a219a3d7498d856195d04
Wim <wim@42.be>
2017-05-01 14:29:51 +0200

    Validate DNSSEC chain on *every* auth NS and parent NS

--- dnssec.go
+++ dnssec.go
@@ -64,7 +64,7 @@ func explicitValid(rr *dns.RRSIG) (int64, int64) {
 
 func validateChain(domain string) (bool, error) {
 	for {
-		log.Debugf("validating %s", domain)
+		log.Debugf("Validating %s", domain)
 		valid, err := validateDomain(domain)
 		if err != nil {
 			return false, err
@@ -82,6 +82,7 @@ func validateChain(domain string) (bool, error) {
 }
 
 func validateDomain(domain string) (bool, error) {
+	// TODO concurrency
 	// get DNSKEY domain.
 	// validate RRSIG on DNSKEY
 	// get DS from parent.
@@ -94,30 +95,43 @@ func validateDomain(domain string) (bool, error) {
 	nsdata, err := findNS(domain)
 	if err != nil {
 	}
-	log.Debugf("Asking NS (%s) DNSKEY of %s", nsdata[0].Info[0].IP.String(), domain)
-	res, err := query(domain, dns.TypeDNSKEY, nsdata[0].Info[0].IP.String(), true)
-	if err != nil {
-		return false, err
-	}
-	// map DNSKEYs
-	for _, a := range res.Msg.Answer {
-		switch a.(type) {
-		case *dns.DNSKEY:
-			key := a.(*dns.DNSKEY)
-			keyMap[key.KeyTag()] = key
+	for _, ns := range nsdata {
+		for _, nsip := range ns.IP {
+			found := false
+			log.Debugf("Asking NS %s (%s) DNSKEY of %s", ns.Name, nsip.String(), domain)
+			res, err := query(domain, dns.TypeDNSKEY, nsip.String(), true)
+			if err != nil {
+				log.Debugf("error %s", err)
+				break
+			}
+			// map DNSKEYs
+			for _, a := range res.Msg.Answer {
+				switch a.(type) {
+				case *dns.DNSKEY:
+					found = true
+					key := a.(*dns.DNSKEY)
+					if exist, ok := keyMap[key.KeyTag()]; ok {
+						if key.PublicKey != exist.PublicKey {
+							return false, fmt.Errorf("Validation failed. DNSKEY with same keytag differ")
+						}
+					}
+					keyMap[key.KeyTag()] = key
+				}
+			}
+			if !found {
+				return false, fmt.Errorf("Validation failed. No DNSKEY found for %s on %s", domain, nsip.String())
+			}
+
+			valid, info, err := validateDNSKEY(res.Msg.Answer)
+			if valid {
+				log.Debugf("RRSIG validated (%s -> %s)", time.Unix(info.Start, 0), time.Unix(info.End, 0))
+			} else {
+				log.Debugf("RRSIG not validated")
+				return false, fmt.Errorf("Validation failed. RRSIG on DNSKEY could not be validated by any DNSKEY for %s", domain)
+			}
 		}
 	}
-	if len(keyMap) == 0 {
-		return false, fmt.Errorf("Validation failed. No DNSKEY found for %s on %s", domain, nsdata[0].Info[0].IP.String())
-	}
-
-	valid, info, err := validateDNSKEY(res.Msg.Answer)
-	if valid {
-		log.Debugf("RRSIG validated (%s -> %s)", time.Unix(info.Start, 0), time.Unix(info.End, 0))
-	} else {
-		log.Debugf("RRSIG not validated")
-		return false, fmt.Errorf("Validation failed. RRSIG on DNSKEY could not be validated by any DNSKEY for %s", domain)
-	}
+	log.Debugf("Found %v valid DNSKEY for %s", len(keyMap), domain)
 
 	// get auth servers of parent
 	log.Debugf("Finding NS of parent: %s", dns.Fqdn(getParentDomain(domain)))
@@ -126,39 +140,57 @@ func validateDomain(domain string) (bool, error) {
 	}
 
 	// asking parent about DS
-	log.Debugf("Asking parent %s (%s) DS of %s", nsdata[0].Info[0].IP.String(), getParentDomain(domain), domain)
-	res, err = query(domain, dns.TypeDS, nsdata[0].Info[0].IP.String(), true)
-	if err == nil && len(res.Msg.Answer) == 0 {
-		return false, fmt.Errorf("Validation failed. No DS records found for %s on %v\n", domain, nsdata[0].Info[0].IP.String())
-	}
-
-	// look for all parent DS and compare digests
-	for _, a := range res.Msg.Answer {
-		switch a.(type) {
-		case *dns.DS:
-			parentDS := a.(*dns.DS)
-			// does the child has a DNSKEY with the found KeyTag ?
-			key := keyMap[parentDS.KeyTag]
-			if key == nil {
-				log.Debugf("No DNSKEY (keytag %v) in %s found that matches DS (keytag %v) in %s", parentDS.KeyTag, domain, parentDS.KeyTag, nsdata[0].Info[0].IP.String())
-				continue
+	foundKeyTag := false
+	for _, ns := range nsdata {
+		for _, nsip := range ns.IP {
+			log.Debugf("Asking parent %s (%s) DS of %s", ns.Name, nsip.String(), domain)
+			res, err := query(domain, dns.TypeDS, nsip.String(), true)
+			if err == nil && len(res.Msg.Answer) == 0 {
+				return false, fmt.Errorf("Validation failed. No DS records found for %s on %v\n", domain, nsip.String())
 			}
-			// create the child digest based on the parentDS digesttype
-			childDS := key.ToDS(parentDS.DigestType)
-			// if this doesn't fail (shouldn't be happening?)
-			if childDS != nil {
-				log.Debugf("parent DS digest: %s (keytag %v)", parentDS.Digest, parentDS.KeyTag)
-				log.Debugf("child DS digest %s (keytag %v)", childDS.Digest, childDS.KeyTag)
-				if parentDS.Digest == childDS.Digest {
-					log.Debugf("%s validated\n", domain)
-					return true, nil
-				} else {
-					log.Debugf("%s failure", domain)
+			if err != nil {
+				log.Debugf("error %s", err)
+				break
+			}
+			// look for all parent DS and compare digests
+			for _, a := range res.Msg.Answer {
+				switch a.(type) {
+				case *dns.DS:
+					parentDS := a.(*dns.DS)
+					// does the child has a DNSKEY with the found KeyTag ?
+					key := keyMap[parentDS.KeyTag]
+					if key == nil {
+						log.Debugf("No DNSKEY (keytag %v) in %s found that matches DS (keytag %v) in %s", parentDS.KeyTag, domain, parentDS.KeyTag, nsip.String())
+						continue
+					}
+					if parentDS.DigestType == 3 {
+						// no support for GOST for now
+						break
+					}
+					foundKeyTag = true
+					// create the child digest based on the parentDS digesttype
+					childDS := key.ToDS(parentDS.DigestType)
+					// if this doesn't fail (shouldn't be happening?)
+					if childDS != nil {
+						log.Debugf("parent DS digest: %s (keytag %v, type %v)", parentDS.Digest, parentDS.KeyTag, parentDS.DigestType)
+						log.Debugf("child DS digest %s (keytag %v, type %v)", childDS.Digest, childDS.KeyTag, childDS.DigestType)
+						if parentDS.Digest == childDS.Digest {
+							log.Debugf("%s validated", domain)
+							//return true, nil
+						} else {
+							log.Debugf("%s failure", domain)
+							return false, nil
+						}
+					} else {
+						log.Debugf("childDS is nil ? shouldn't be happening %v %v %v", parentDS.KeyTag, key.PublicKey, parentDS.DigestType)
+					}
 				}
-			} else {
-				log.Debugf("childDS is nil ? shouldn't be happening")
 			}
 		}
 	}
-	return false, nil
+	if !foundKeyTag {
+		log.Debugf("Validation failed. No DNSKEY in %s found that matches DS in %s", domain, getParentDomain(domain))
+		return false, fmt.Errorf("Validation failed. No DNSKEY in %s found that matches DS in %s", domain, getParentDomain(domain))
+	}
+	return true, nil
 }
