6f0822ea8c144e564f629e97bfac27fa7c78c1c0
Wim <wim@42.be>
2017-05-04 23:19:55 +0200

    Add cname, reverse check (MXcheck)

--- mx.go
+++ mx.go
@@ -104,6 +104,80 @@ func (c *MXCheck) checkDuplicateIP() map[string][]string {
 	return m
 }
 
+func (c *MXCheck) CheckCNAME() []ReportResult {
+	rep := []ReportResult{}
+	m := make(map[string]bool)
+	for _, mx := range c.MX {
+		if len(mx.MX) > 0 {
+			for mxName := range mx.MXIP {
+				// skip lookup if already done
+				if _, ok := m[mxName]; ok {
+					break
+				}
+				res, err := query(dns.Fqdn(mxName), dns.TypeA, resolver, true)
+				if err != nil {
+					break
+				}
+				cname := extractRR(res.Msg.Answer, dns.TypeCNAME)
+				if len(cname) > 0 {
+					rep = append(rep, ReportResult{Result: fmt.Sprintf("FAIL: Your MX (%s) is a CNAME.", mxName),
+						Status: false})
+				}
+				res, err = query(dns.Fqdn(mxName), dns.TypeAAAA, resolver, true)
+				if err != nil {
+					break
+				}
+				cname = extractRR(res.Msg.Answer, dns.TypeCNAME)
+				if len(cname) > 0 {
+					rep = append(rep, ReportResult{Result: fmt.Sprintf("FAIL: Your MX (%s) is a CNAME.", mxName),
+						Status: false})
+				}
+				m[mxName] = true
+			}
+		}
+	}
+	if len(rep) == 0 {
+		rep = append(rep, ReportResult{Result: "OK  : No CNAMEs found for your MX records",
+			Status: true})
+	}
+	return rep
+}
+
+func (c *MXCheck) CheckReverse() []ReportResult {
+	rep := []ReportResult{}
+	m := make(map[string]bool)
+	for _, mx := range c.MX {
+		if len(mx.MXIP) > 0 {
+			for name, ips := range mx.MXIP {
+				for _, ip := range ips {
+					rev, _ := dns.ReverseAddr(ip.String())
+					res, _, err := queryRRset(rev, dns.TypePTR, resolver, true)
+					if err != nil {
+						break
+					}
+					if len(res) > 0 {
+						m[name] = true
+					} else {
+						m[name] = false
+					}
+				}
+			}
+			break
+		}
+	}
+	for name, reverse := range m {
+		if !reverse {
+			rep = append(rep, ReportResult{Result: fmt.Sprintf("WARN: Reverse PTR lookup for MX %s failed.", name),
+				Status: false})
+		}
+	}
+	if len(rep) == 0 {
+		rep = append(rep, ReportResult{Result: "OK  : All MX records have reverse PTR records",
+			Status: true})
+	}
+	return rep
+}
+
 func (c *MXCheck) Values() []ReportResult {
 	var results []ReportResult
 	var rrset []dns.RR
@@ -158,4 +232,6 @@ func (c *MXCheck) CreateReport(domain string) {
 	c.Report.Type = "MX"
 	c.Report.Result = append(c.Report.Result, c.Identical())
 	c.Report.Result = append(c.Report.Result, c.Values()...)
+	c.Report.Result = append(c.Report.Result, c.CheckCNAME()...)
+	c.Report.Result = append(c.Report.Result, c.CheckReverse()...)
 }
